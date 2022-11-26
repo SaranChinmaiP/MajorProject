37f3124911f6e4ecd0435615be8a77f4c3bcd7ed
Saran P <95578805+SaranChinmaiP@users.noreply.github.com>
2022-11-26 14:28:03 +0530

    Code Fixes
    Added Rectangular Box and Crop Images

--- scripts/index.js
+++ scripts/index.js
@@ -1,19 +1,26 @@
 
 
-//const URL = "https://teachablemachine.withgoogle.com/models/6xm60hr9Q/";
-const URL =  "https://teachablemachine.withgoogle.com/models/9Jr_sKu8X/"
+// 50 EPOCH const URL = "https://teachablemachine.withgoogle.com/models/6xm60hr9Q/";
+// 100 EPOCHS const URL = "https://teachablemachine.withgoogle.com/models/9Jr_sKu8X/"
+const URL = "https://teachablemachine.withgoogle.com/models/e3BmwIkIA/";
+// 500 EPOCHs
 
-
-
+var handIsThere = false;
 console.log("Hands.js Script is Runnig");
 const video3 = document.getElementsByClassName('input_video3')[0];
 const out3 = document.getElementsByClassName('webcam-container')[0];
-const controlsElement3 = document.getElementsByClassName('control3')[0];
 const canvasCtx3 = out3.getContext('2d');
+const out1 = document.getElementsByClassName('crop-container')[0];
+const crop = out1.getContext('2d');
+const controlsElement3 = document.getElementsByClassName('control3')[0];
 const fpsControl = new FPS();
+const STATUS_BOX = document.getElementById('status');
 
 
 function onResultsHands(results) {
+  {
+    crop.drawImage(out3,180,60,180,235,0,0,190,235);
+  }
   document.body.classList.add('loaded');
   fpsControl.tick();
 
@@ -21,7 +28,15 @@ function onResultsHands(results) {
   canvasCtx3.clearRect(0, 0, out3.width, out3.height);
   canvasCtx3.drawImage(
     results.image, 0, 0, out3.width, out3.height);
+    {
+      canvasCtx3.beginPath();
+      canvasCtx3.rect(180, 60, 180, 235);   
+      canvasCtx3.lineWidth = 0.5;
+      canvasCtx3.strokeStyle = 'black';
+      canvasCtx3.stroke();
+    }
   if (results.multiHandLandmarks && results.multiHandedness) {
+    handIsThere = true;
     for (let index = 0; index < results.multiHandLandmarks.length; index++) {
       const classification = results.multiHandedness[index];
       const isRightHand = classification.label === 'Right';
@@ -37,6 +52,9 @@ function onResultsHands(results) {
           }
         });
     }
+  }
+  else {
+    handIsThere = false
   }
   canvasCtx3.restore();
 }
@@ -86,7 +104,15 @@ new ControlPanel(controlsElement3, {
     video3.classList.toggle('selfie', options.selfieMode);
     hands.setOptions(options);
   });
-
+  
+//<--------------------------------------------------------------------------------------->
+{
+  canvasCtx3.beginPath();
+  canvasCtx3.rect(180, 30, 190, 225);   
+  canvasCtx3.lineWidth = 0.5;
+  canvasCtx3.strokeStyle = 'black';
+  canvasCtx3.stroke();
+}
 //<--------------------------------------------------------------------------------------->
 
 console.log("Script.js is Runnig");
@@ -97,6 +123,7 @@ const WIDTH = 375, HEIGHT = 410;
 var speechSwitch = document.getElementById("SpeechSwitch");
 var isChecked = document.getElementById("GraphSwitch");
 var gdata = [["Element", "%", { role: "style" }], ["Label", 50, "gold"]];
+
 
 
 // Load the image model and setup the webcam
@@ -116,9 +143,8 @@ async function init() {
   window.requestAnimationFrame(loop);
 
   // append elements to the DOM
-  
-}
 
+}
 
 async function PauseCam() {
   video3.pause(); // Pause the webcam frame
@@ -131,14 +157,21 @@ async function PlayCam() {
 
 async function loop() {
   webcam.update(); // update the webcam frame
-  await predict();
+  if (handIsThere) {
+    await predict();
+  }
+  else{
+    STATUS_BOX.innerText = "Hand🤚Not❎Detected " ;
+
+  }
   window.requestAnimationFrame(loop);
 }
 
 // run the webcam image through the image model
+
 async function predict() {
   // predict can take in an image, video or canvas html element
-  const prediction = await model.predict(out3);
+  const prediction = await model.predict(out1);
 
   // Gets the Maximum Prediction of the Probabilty
   var max = Math.max.apply(null,
@@ -160,10 +193,15 @@ async function predict() {
     //Finds the Class Name of the  maximum value
     const index = prediction.findIndex((element) => element.probability === max);
     console.log(prediction[index]['className'] + ' : ' + Factor + "%");
-    document.getElementById('status').innerText = "Sign : " + prediction[index]['className'] + ' : ' + Factor + "% (confidence rate)";
+    
+    STATUS_BOX.innerText = "Sign : " + prediction[index]['className'] + ' : ' + Factor + "% (confidence rate)";
+   
     words[0] = prediction[index]['className'];
   }
+  if(Factor < 55){
+    STATUS_BOX.innerText = "🤔💭Try to fit the Hand in the Box✊";
 
+  }
 
   if ((speechSwitch.checked == true) && words[1] != words[0] && words[0] != undefined) {
     console.log("I am Speaking");
